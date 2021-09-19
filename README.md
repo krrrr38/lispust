7876c3a00493087512473725effc35262b307f25
krrrr38 <k.kaizu38@gmail.com>
2021-09-19 19:12:55 +0900

    http server

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # lispust
 
 toy lisp-ish language
+
+## http-server
+
+```sh
+> cargo run -p http-server
+```
+
+Now you can post from other tty
+```
+> curl -XPOST -d'(+ 3 5)' localhost:8080/lispust                        (git)-[master] -
+Number(8)
+```
---
+++ http-server/src/main.rs
@@ -0,0 +1,26 @@
+extern crate core;
+
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("ok")
+}
+
+#[post("/lispust")]
+async fn lispust(req_body: String) -> impl Responder {
+    match core::run(&req_body) {
+        Ok(ret) => HttpResponse::Ok().body(ret),
+        Err(e) => HttpResponse::BadRequest().body(e.to_string()),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let addr = "127.0.0.1:8080";
+    println!("start server on {}", addr);
+    HttpServer::new(|| App::new().service(health).service(lispust))
+        .bind(addr)?
+        .run()
+        .await
+}
